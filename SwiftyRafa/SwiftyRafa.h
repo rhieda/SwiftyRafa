85ba0d2cec26a33c9fb6b171bb59ffc515299815
Rafael <41241789@mackenzista.com.br>
2020-01-20 21:32:08 -0300

    Initial Commit

---
+++ SwiftyRafa/SwiftyRafa.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRafa.h
+//  SwiftyRafa
+//
+//  Created by Rafael  Hieda on 1/20/20.
+//  Copyright © 2020 DevelopersUnited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyRafa.
+FOUNDATION_EXPORT double SwiftyRafaVersionNumber;
+
+//! Project version string for SwiftyRafa.
+FOUNDATION_EXPORT const unsigned char SwiftyRafaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRafa/PublicHeader.h>
+
+
